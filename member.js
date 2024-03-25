f2af77112e6512c7908834f321fa5c37b5dcd76b
David Rueda Barrón <crounly@gmail.com>
2024-03-25 18:05:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+
+  member.style.display = "none";
+  skills.style.display = "block";
+  memberBtn.style.backgroundColor = "#f1f1f1";
+  skillsBtn.style.backgroundColor = "#f1f1f1";
+}
--- skills.js
+++ skills.js
@@ -1,6 +1,3 @@
 function calculateNumbers( var1, var2 ) {
     console.log(var1 + var2);
 }
-function calculateNumbers2( var1, var2 ) {
-    console.log(var1 + var2);
-}
